54a7f32f071ca968f23525030400caf49566e4b9
Abraão Barbosa <abraaobarbosati@gmail.com>
2021-04-23 23:32:05 -0300

    Correções em estilizações e adicionada função de contador de visualização de posts e exibição na home page

--- portal/views.py
+++ portal/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import Post
 
 # Create your views here.
 def home(request):
-    posts = Post.objects.all()
-    return render(request, 'guest/index.html', {'posts': posts})
+    posts = Post.objects.all().order_by('-views')[:3:1]
+    users = User.objects.all() 
+    return render(request, 'guest/index.html', {'posts': posts, 'users': users.count()})
 
 def project(request, post_slug):
     post = Post.objects.get(slug=post_slug)
+    post.views = post.views + 1
+    post.save()
     return render(request, 'guest/project.html', {'post': post})
-
