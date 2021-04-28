4d0a38399e58b4f1e2be4565c5fe22f3801f7618
Abraão Barbosa <abraaobarbosati@gmail.com>
2021-04-28 15:19:50 -0300

    Config project of deploy

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(config['BASE_DIR'], 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
