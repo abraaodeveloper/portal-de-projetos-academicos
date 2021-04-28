30c64f7b4462c0b0af9a9f3eda07706988905e6a
Abraão Barbosa <abraaobarbosati@gmail.com>
2021-04-28 15:24:00 -0300

    Config project of deploy

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Configure Django App for Heroku.
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +137,5 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 LOGIN_REDIRECT_URL = '/dashboard'
 
 LOGOUT_REDIRECT_URL = '/accounts/login'
+
+django_heroku.settings(locals())
