0e4c67686ab38a1bd90a8498d40b3a2625b0c5d0
Abraão Barbosa <abraaobarbosati@gmail.com>
2021-04-29 17:23:44 -0300

    mege by main

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -40,9 +40,9 @@ window.onclick = function (event) {
 
 /// form cadaster
 
-const ik =  document.getElementById("id_keyword")
-const iqp =  document.getElementById("id_qtd_pages")
-const itc =  document.getElementById("id_type_content")
+const ik = document.getElementById("id_keyword")
+const iqp = document.getElementById("id_qtd_pages")
+const itc = document.getElementById("id_type_content")
 /*
 ik.style.display = "none"
 iqp.style.display = "none"
@@ -57,3 +57,36 @@ itc.onchange = function(){
     }
 };
 */
+
+
+// === include 'setup' then 'config' above ===
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
