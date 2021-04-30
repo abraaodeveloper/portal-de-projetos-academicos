7c059f339afbd85306085a0fd242bb050329d6b8
Abraão Barbosa <abraaobarbosati@gmail.com>
2021-04-30 19:41:08 -0300

    Fix bug

--- portal/views/guest_views.py
+++ portal/views/guest_views.py
@@ -11,25 +11,29 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
-    softs = Soft.objects.all().order_by('-views')[:3:1]
-    ebooks = Ebook.objects.all().order_by('-views')[:3:1]
-    result = softs + ebooks
 
-    if(not(len(result) < 3)):
-        cont = 0
-        ordenado = False
-        while(cont < len(result)-1 and not(ordenado)):
-            ordenado = True
-            if(result[cont].views < result[cont+1].views):
-                ordenado = False
-                key = result[cont]
-                result[cont] = result[cont+1]
-                result[cont+1] = key
-            cont +=1
+    try: 
+        softs = Soft.objects.all().order_by('-views')[:3:1]
+        ebooks = Ebook.objects.all().order_by('-views')[:3:1]
+        result = softs + ebooks
 
-    users = User.objects.all()
+        if(not(len(result) < 3)):
+            cont = 0
+            ordenado = False
+            while(cont < len(result)-1 and not(ordenado)):
+                ordenado = True
+                if(result[cont].views < result[cont+1].views):
+                    ordenado = False
+                    key = result[cont]
+                    result[cont] = result[cont+1]
+                    result[cont+1] = key
+                cont +=1
 
-    return render(request, 'guest/index.html', {'posts': result[:3:1], 'users': users.count(), 'qtdAcess': 3})
+        users = User.objects.all()
+
+        return render(request, 'guest/index.html', {'posts': result[:3:1], 'users': users.count(), 'qtdAcess': 3})
+    except:
+        return redirect('home')
 
 def projects(request, type_content):
     if (type_content == "software"):
