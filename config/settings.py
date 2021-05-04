560950616478f9002e187d35d020d95bb1cb8d88
Abraão Barbosa <abraaobarbosati@gmail.com>
2021-05-04 18:30:49 -0300

    set debug=false

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2xjc0x_isx@0esm94+ygnwb(b^yfnd%oj29p#m%xzmxa_%pme)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
