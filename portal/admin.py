0b1eb8d8bd13056c6a0323e23d620feaa01e4e17
Abraão Barbosa <abraaobarbosati@gmail.com>
2021-05-04 21:24:25 -0300

    customization of the administration page

--- portal/admin.py
+++ portal/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from portal.models import Soft, Ebook
 
+admin.site.site_header = 'Portal de Projetos Acadêmicos'
+admin.site.index_title = 'Aqui voçê pode gerenciar todos os projetos!'
+admin.site.site_title = 'Administração'
 
 # Register your models here.
 admin.site.register(Soft)
