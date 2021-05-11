a99e6878801efbbe07f5db17c92709cdf683eab7
Abraão Barbosa <abraaobarbosati@gmail.com>
2021-05-11 15:34:24 -0300

    Alter graphic to bar

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -26,8 +26,6 @@ getJSON("/getstatistic",
             let nResultLabels = [rLabels[0]]
             let nResultValues = [0]
 
-            console.log(nResultLabels);
-
             for(i in rLabels){
                 let isEqual = false;
                 let indexEqual = 0;
@@ -44,17 +42,50 @@ getJSON("/getstatistic",
                     nResultValues.push(1); 
                 }
             }
-            
+
+            const orderList = []
+            for (const i in nResultLabels) {
+                let dat = nResultLabels[i].split("-");
+                orderList.push([parseInt(dat[0] + dat[1] + dat[2]), parseInt(i)]);
+            }
+            console.log(orderList);
+            let bubbleSort = (inputArr) => {
+                let len = inputArr.length;
+                let swapped;
+                do {
+                    swapped = false;
+                    for (let i = 0; i < len-1; i++) {
+                        if (inputArr[i][0] > inputArr[i + 1][0]) {
+                            let tmp = inputArr[i];
+                            inputArr[i] = inputArr[i + 1];
+                            inputArr[i + 1] = tmp;
+                            swapped = true;
+                        }
+                    }
+                } while (swapped);
+                return inputArr;
+            };
+
+            let ordenado = bubbleSort(orderList);
+            console.log(ordenado);
+
+            const nLabl = [];
+            const nResl = [];
+            for(i of ordenado){
+                nLabl.push(nResultLabels[i[1]]);
+                nResl.push(nResultValues[i[1]]);
+            }
+
             // chart.js
             const labels = nResultLabels;
 
             const data = {
-                labels: labels,
+                labels: nLabl,
                 datasets: [{
                     label: 'Quantidade de novos projetos por dia',
                     backgroundColor: 'rgb(255, 99, 132)',
                     borderColor: 'rgb(255, 99, 132)',
-                    data: nResultValues,
+                    data: nResl,
                 }]
             };
 
